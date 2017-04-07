fd9fdffe567d484f309f3cc7e0259f15534f3627
jakimychev.nikolaj <jakimychev.nikolaj@gmail.com>
2017-04-07 15:31:48 +0300

    work with forms

--- yii_framework/basic/controllers/SiteController.php
+++ yii_framework/basic/controllers/SiteController.php
@@ -2,14 +2,27 @@
 
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
+use app\models\EntryForm;
 
 class SiteController extends Controller
 {
     // ...существующий код...
 
-    public function actionSay($message = 'Привет')
+    public function actionEntry()
     {
-        return $this->render('say', ['message' => $message]);
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // данные в $model удачно проверены
+
+            // делаем что-то полезное с $model ...
+
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // либо страница отображается первый раз, либо есть ошибка в данных
+            return $this->render('entry', ['model' => $model]);
+        }
     }
 }
