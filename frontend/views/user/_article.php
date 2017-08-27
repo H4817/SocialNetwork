b476490ab5590ff543cd78022db9ed96abd07419
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2017-08-27 16:41:18 +0300

    change js implementation for articles

--- frontend/views/user/_article.php
+++ frontend/views/user/_article.php
@@ -2,72 +2,70 @@
 use yii\widgets\ActiveForm;
 use yii\widgets\ListView;
 
-$randomStr = \Yii::$app->security->generateRandomString();
 ?>
 
-<div class="well">
-    <div id="post_<?= $randomStr ?>">
-        <?= $model->content ?> <br> <br>
-        <img src="../../common/uploads/<?= $model->imageReference ?>" alt="err" class="img-responsive"> <br>
-        <?= $model->date ?>
-        <?php if (!\Yii::$app->user->isGuest): ?>
-            <?php if (Yii::$app->user->id === $model->userId): ?>
-                <?= Html::a('<span class="glyphicon glyphicon-trash"></span>', ['post/delete', 'id' => $model->postId],
-                    [
-                        'class' => 'btn btn-danger',
-                        'style' => 'float: right',
-                        'data' => [
-                            'confirm' => 'Are you sure you want to delete this item?',
-                            'method' => 'post',
-                        ],
-                    ]) ?>
-                <div class="btn btn-primary" id="edit_<?= $randomStr ?>" style="float: right; margin-right: 10px;"><span
-                            class="glyphicon glyphicon-pencil"></span></div>
-                <br> <br>
+<div class="well post-container">
+    <div class="post-item">
+        <div class="post">
+
+            <?= $model->content ?> <br> <br>
+            <img src="../../common/uploads/<?= $model->imageReference ?>" alt="err" class="img-responsive"> <br>
+            <?= $model->date ?>
+            <?php if (!\Yii::$app->user->isGuest): ?>
+                <?php if (Yii::$app->user->id === $model->userId): ?>
+                    <?= Html::a('<span class="glyphicon glyphicon-trash"></span>',
+                        ['post/delete', 'id' => $model->postId],
+                        [
+                            'class' => 'btn btn-danger',
+                            'style' => 'float: right',
+                            'data' => [
+                                'confirm' => 'Are you sure you want to delete this item?',
+                                'method' => 'post',
+                            ],
+                        ]) ?>
+                    <div class="btn btn-primary edit-post-button" style="float: right; margin-right: 10px;"><span
+                                class="glyphicon glyphicon-pencil"></span></div>
+                    <br> <br>
+                <?php endif ?>
             <?php endif ?>
-        <?php endif ?>
 
-
-        <?php if (!\Yii::$app->user->isGuest): ?>
-            <?php \yii\widgets\Pjax::begin(['timeout' => 5000]); ?>
-            <?php $form =
-                ActiveForm::begin(['options' => ['class' => 'comment-form'], 'action' => ['comment/create']]); ?>
-            <?= $form->field($comment, 'message')->textarea(['rows' => 6]); ?>
-            <?= $form->field($comment, 'postId')->hiddenInput(['value' => $model->postId])->label(false) ?>
-            <div class="form-group">
-                <?= Html::submitButton('Send', ['class' => 'btn btn-primary']); ?>
-            </div>
-            <?php $form = ActiveForm::end(); ?>
-            <?php \yii\widgets\Pjax::end(); ?>
-        <?php endif ?>
-
-    </div>
-
-    <div id="form_<?= $randomStr ?>" style="display: none;">
-        <?php $form =
-            ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data'],
-                'action' => ['post/update', 'id' => $model->postId]]); ?>
-        <?= $form->field($model, 'imageFile')->fileInput() ?>
-        <?= $form->field($model, 'content')->textarea(['rows' => 6]); ?>
-        <div class="form-group">
-            <?= Html::submitButton('Update', ['class' => 'btn btn-primary']); ?>
-            <span class="btn btn-danger" id="cancel_<?= $randomStr ?>">Cancel</span>
+            <?php if (!\Yii::$app->user->isGuest): ?>
+                <?php \yii\widgets\Pjax::begin(['timeout' => 5000]); ?>
+                <?php $form =
+                    ActiveForm::begin(['options' => ['class' => 'comment-form'], 'action' => ['comment/create']]); ?>
+                <?= $form->field($comment, 'message')->textarea(['rows' => 6]); ?>
+                <?= $form->field($comment, 'postId')->hiddenInput(['value' => $model->postId])->label(false) ?>
+                <div class="form-group">
+                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary']); ?>
+                </div>
+                <?php $form = ActiveForm::end(); ?>
+                <?php \yii\widgets\Pjax::end(); ?>
+            <?php endif ?>
         </div>
-        <?php ActiveForm::end(); ?>
+
+        <div class="post-form" style="display: none;">
+            <?php $form =
+                ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data'],
+                    'action' => ['post/update', 'id' => $model->postId]]); ?>
+            <?= $form->field($model, 'imageFile')->fileInput() ?>
+            <?= $form->field($model, 'content')->textarea(['rows' => 6]); ?>
+            <div class="form-group">
+                <?= Html::submitButton('Update', ['class' => 'btn btn-primary']); ?>
+                <span class="btn btn-danger cancel-button">Cancel</span>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+
     </div>
-
     <script type="text/javascript">
-        var editPost = document.getElementById("edit_<?=$randomStr?>");
-        var cancelEditing = document.getElementById("cancel_<?=$randomStr?>");
-
-        editPost.onclick = function () {
-            $('#post_<?=$randomStr?>').hide();
-            $('#form_<?=$randomStr?>').show();
-        };
-        cancelEditing.onclick = function () {
-            $('#post_<?=$randomStr?>').show();
-            $('#form_<?=$randomStr?>').hide();
-        };
+        $(".post-item .edit-post-button").click(function () {
+            $(this).closest('.post').hide();
+            $(this).parent('.post').parent('.post-item').find('.post-form').show();
+        });
+        $(".post-form .cancel-button").click(function () {
+            $(this).closest('.post-form').hide();
+            $(this).closest('.form-group').closest('.post-form').closest('.post-item').children('.post').show();
+        });
     </script>
 
     <?php \yii\widgets\Pjax::begin(['timeout' => 5000]); ?>
